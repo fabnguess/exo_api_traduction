7d53a26d63ca1c6f1baba59c6b695e390a9fc2ed
Fabrice <fabnguess@gmail.com>
2022-02-10 17:34:29 +0000

    Fixé la traduction en francais

--- serveur.js
+++ serveur.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const translate = require('translate')
+const express = require("express")
+const translate = require("translate")
 
 const app = express()
 app.use(express.json())
 
-app.post('/', async(req, res) => {
-    const { texte, cible } = req.body
-    const traduction = await translate(texte, { from: 'fr', to: cible })
-    res.send(traduction)
+app.post("/", async (req, res) => {
+  const { texte } = req.body
+  const traduction = await translate(texte, "fr")
+  res.send(traduction)
 })
 
 app.listen(3000)
-console.log('Serveur en marche');
+console.log("Serveur en marche")
