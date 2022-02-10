5235a0a880e0849af5e03a971a52c6f578ca89a4
Fabrice <fabnguess@gmail.com>
2022-02-10 21:15:57 +0000

    Parametrage de l'api

--- serveur.js
+++ serveur.js
@@ -4,12 +4,12 @@ import translate from "translate"
 const serveur = Fastify()
 
 serveur.post("/", async (req, res) => {
-  const { texte } = req.body
-  const traduction = await translate(texte, "fr")
+  const { texte, source, cible } = req.body
+  const traduction = await translate(texte, { from: source, to: cible })
   res.send(traduction)
 })
 
-serveur.listen(process.env.PORT || 3000, (err, address) => {
+serveur.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
   if (err) throw err
   console.log(`Le serveur écoute maintenant ${address}`)
 })
