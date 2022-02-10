f48abd22b0bcbab7ea84f3b7aa99d4873470b9fb
Fabrice <fabnguess@gmail.com>
2022-02-10 21:19:33 +0000

    Le hostname en localhost

--- serveur.js
+++ serveur.js
@@ -5,11 +5,12 @@ const serveur = Fastify()
 
 serveur.post("/", async (req, res) => {
   const { texte, source, cible } = req.body
+
   const traduction = await translate(texte, { from: source, to: cible })
   res.send(traduction)
 })
 
-serveur.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
+serveur.listen(process.env.PORT || 3000, "localhost", (err, address) => {
   if (err) throw err
   console.log(`Le serveur écoute maintenant ${address}`)
 })
